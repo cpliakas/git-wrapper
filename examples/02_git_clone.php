007cd177c5144f68b433f24dca585e9bb02c95a0
TomasVotruba <tomas.vot@gmail.com>
2013-01-09 14:54:30 -0500

    Modified example URL.

--- examples/02_git_clone.php
+++ examples/02_git_clone.php
@@ -6,5 +6,5 @@ use GitWrapper\Command\GitClone;
 require_once __DIR__ . '/../vendor/autoload.php';
 $git = new GitWrapper();
 
-$clone = new GitClone('https://github.com/cpliakas/git-wrapper.git');
+$clone = new GitClone('git://github.com/cpliakas/git-wrapper.git');
 $git->run($clone);
