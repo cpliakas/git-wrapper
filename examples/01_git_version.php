f15c05e3cdd314db82b7d4540e56cfd51d34e7ac
TomasVotruba <tomas.vot@gmail.com>
2013-01-10 00:28:39 -0500

    Added a fluent interface.

--- examples/01_git_version.php
+++ examples/01_git_version.php
@@ -3,6 +3,6 @@
 use GitWrapper\GitWrapper;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-$git = new GitWrapper();
 
+$git = new GitWrapper();
 print $git->version();
--- src/GitWrapper/GitWorkingCopy.php
+++ src/GitWrapper/GitWorkingCopy.php
@@ -11,6 +11,8 @@
 namespace GitWrapper;
 
 use GitWrapper\Command\GitAdd;
+use GitWrapper\Command\GitClone;
+use GitWrapper\Command\GitCommandAbstract;
 use GitWrapper\Command\GitCommit;
 use GitWrapper\Command\GitInit;
 use GitWrapper\Command\GitPush;
@@ -31,6 +33,11 @@ class GitWorkingCopy
      * @var string
      */
     protected $_directory;
+
+    /**
+     * The output captured by the last run commnd(s).
+     */
+    protected $_output = '';
 
     /**
      * Constructs a GitWorkingCopy object.
@@ -61,42 +68,85 @@ class GitWorkingCopy
     }
 
     /**
+     * @return string
+     */
+    public function getOutput()
+    {
+        $output = $this->_output;
+        $this->_output = '';
+        return $output;
+    }
+
+    /**
+     * @return GitWorkingCopy
+     */
+    public function clearOutput()
+    {
+        $this->_output = '';
+        return $this;
+    }
+
+    /**
+     * @param GitCommandAbstract $command
+     * @param array $options
+     */
+    public function run(GitCommandAbstract $command, array $options)
+    {
+        $command->setOptions($options);
+        $this->_output .= $this->_wrapper->run($command);
+        return $this;
+    }
+
+    /**
      * @param string $filepattern
      * @param array $options
+     * @return GitWorkingCopy
      *
-     * @see GitAdd::__construct()
+     * @see GitAdd
      */
     public function add($filepattern, array $options = array())
     {
         $add = new GitAdd($this->_directory, $filepattern);
-        $add->setOptions($options);
-        return $this->_wrapper->run($add);
+        return $this->run($add, $options);
+    }
+
+    /**
+     * @param string $repository
+     * @param array $options
+     * @return GitWorkingCopy
+     *
+     * @see GitClone
+     */
+    public function cloneRepository($repository, array $options = array())
+    {
+        $clone = new GitClone($repository, $this->_directory);
+        return $this->run($clone, $options);
     }
 
     /**
      * @param string|null $log_message
      * @param string|null $files
      * @param array $options
+     * @return GitWorkingCopy
      *
-     * @see GitCommit::__construct()
+     * @see GitCommit
      */
     public function commit($log_message = null, $files = null, array $options = array())
     {
         $commit = new GitCommit($this->_directory, $log_message, $files);
-        $commit->setOptions($options);
-        return $this->_wrapper->run($commit);
+        return $this->run($commit, $options);
     }
 
     /**
      * @param array $options
+     * @return GitWorkingCopy
      *
-     * @see GitInit::__construct()
+     * @see GitInit
      */
     public function init(array $options = array())
     {
         $init = new GitInit($this->_directory);
-        $init->setOptions($options);
-        return $this->_wrapper->run($init);
+        return $this->run($init, $options);
     }
 
     /**
@@ -104,11 +154,27 @@ class GitWorkingCopy
      *   The "remote" repository that is destination of a push operation.
      * @param string|null $refspec
      * @param array $options
+     * @return GitWorkingCopy
+     *
+     * @see GitPush
      */
     public function push($repository = null, $refspec = null, array $options = array())
     {
         $push = new GitPush($this->_directory, $repository, $refspec);
-        $push->setOptions($options);
-        return $this->_wrapper->run($push);
+        return $this->run($push, $options);
+    }
+
+    /**
+     * Hackish, allows us to use "clone" as a method name.
+     */
+    public function __call($method, $args)
+    {
+        if ('clone' == $method) {
+            return call_user_func_array(array($this, 'cloneRepository'), $args);
+        } else {
+            $class = get_called_class();
+            $message = "Call to undefined method $class::$method()";
+            throw new \BadMethodCallException($message);
+        }
     }
 }
