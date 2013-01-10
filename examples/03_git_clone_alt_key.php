7d3ee4d78ccfa2bf48bb57cffaa7e569b03e98ec
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2013-01-10 17:14:46 -0500

    Fixed #4: Implemented a setPrivateKey() method to simplify using the GIT_SSH wrapper.

--- examples/03_git_clone_alt_key.php
+++ examples/03_git_clone_alt_key.php
@@ -1,13 +1,10 @@
 <?php
 
 use GitWrapper\GitWrapper;
-use GitWrapper\EventListener\GitSSHListener;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 $git = new GitWrapper();
-
-$listener = new GitSSHListener('/path/to/private/key');
-$git->getDispatcher()->addListener(GitEvents::GIT_CLONE, array($listener, 'onGitCommand'));
+$git->setPrivateKey('/path/to/private/key');
 
 $working_copy = $git->workingCopy('./git-wrapper');
 $working_copy->clone('git@github.com:cpliakas/git-wrapper.git');
