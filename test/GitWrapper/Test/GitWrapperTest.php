06dfe2900af25a09ef3972ab45c570e2d6c7778b
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2013-01-20 16:01:53 -0500

    Added skeleton test classes and Travis CI files.

---
+++ test/GitWrapper/Test/GitWrapperTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace GitWrapper\Test;
+
+use GitWrapper\GitWrapper;
+use GitWrapper\Test\Event\TestDispatcher;
+
+class GitWrapperTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testSetGitBinary()
+    {
+        $wrapper = new GitWrapper();
+        $binary = '/path/to/binary';
+        $wrapper->setGitBinary($binary);
+        $this->assertEquals($binary, $wrapper->getGitBinary());
+    }
+
+    public function testSetDispatcher()
+    {
+        $wrapper = new GitWrapper();
+        $dispatcher = new TestDispatcher();
+        $wrapper->setDispatcher($dispatcher);
+        $this->assertEquals($dispatcher, $wrapper->getDispatcher());
+    }
+
+    public function testSetTimeout()
+    {
+        $wrapper = new GitWrapper();
+        $timeout = mt_rand(1, 60);
+        $wrapper->setTimeout($timeout);
+        $this->assertEquals($timeout, $wrapper->getTimeout());
+    }
+
+    public function testGitVersion()
+    {
+        $wrapper = new GitWrapper();
+        $version = $wrapper->git('--version');
+        $this->assertNotEmpty($version);
+    }
+}
