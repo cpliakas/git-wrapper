1edae437a172268c3944c15a9d11e2e108dea3e8
TomasVotruba <tomas.vot@gmail.com>
2013-01-20 16:17:59 -0500

    Added skeleton classes.

---
+++ test/GitWrapper/Test/Event/TestListener.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace GitWrapper\Test\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class TestListener
+{
+    public function onCommand(Event $event)
+    {
+
+    }
+
+    public function onSuccess(Event $event)
+    {
+
+    }
+
+    public function onError(Event $event)
+    {
+
+    }
+}
