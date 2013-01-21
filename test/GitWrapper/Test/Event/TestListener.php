2743861d3aa4401e61196ee1631afcfa8dd9d0bc
TomasVotruba <tomas.vot@gmail.com>
2013-01-20 21:50:01 -0500

    Added command failure tests, separated some base methods into a GitWrapperTestCase class.

--- test/GitWrapper/Test/Event/TestListener.php
+++ test/GitWrapper/Test/Event/TestListener.php
@@ -6,18 +6,30 @@ use Symfony\Component\EventDispatcher\Event;
 
 class TestListener
 {
+    /**
+     * The methods that were called.
+     *
+     * @var array
+     */
+    protected $_methods = array();
+
+    public function methodCalled($method)
+    {
+        return in_array($method, $this->_methods);
+    }
+
     public function onCommand(Event $event)
     {
-
+        $this->_methods[] = 'onCommand';
     }
 
     public function onSuccess(Event $event)
     {
-
+        $this->_methods[] = 'onSuccess';
     }
 
     public function onError(Event $event)
     {
-
+        $this->_methods[] = 'onError';
     }
 }
