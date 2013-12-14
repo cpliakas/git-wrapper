d9cc72225eb691a460530d3bf345ef323d17c508
TomasVotruba <tomas.vot@gmail.com>
2013-12-14 08:35:45 -0500

    Fixed #72: Change to MIT license.

--- src/GitWrapper/Event/GitOutputEvent.php
+++ src/GitWrapper/Event/GitOutputEvent.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * A PHP wrapper around the Git command line utility.
- *
- * @license GNU General Public License, version 3
- * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @see https://github.com/cpliakas/git-wrapper
- * @copyright Copyright (c) 2013 Acquia, Inc.
- */
-
 namespace GitWrapper\Event;
 
 use GitWrapper\GitCommand;
