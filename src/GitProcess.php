d1aab707a3ca62ec8d2886e69a1053e7cc5f1e97
TomasVotruba <tomas.vot@gmail.com>
2018-07-04 19:57:53 +0800

    Fix cs

--- src/GitProcess.php
+++ src/GitProcess.php
@@ -25,25 +25,20 @@ final class GitProcess extends Process
         $this->gitCommand = $gitCommand;
 
         // Build the command line options, flags, and arguments.
-        $gitCommandLine = $gitCommand->getCommandLine();
+        $commandLine = $gitCommand->getCommandLine();
         $gitBinary = $gitWrapper->getGitBinary();
-        if (is_string($gitCommandLine)) {
+        if (is_string($commandLine)) {
             // Support for executing an arbitrary git command.
-            $commandLine = '"' . $gitBinary . '" ' . $gitCommandLine;
+            $commandLine = '"' . $gitBinary . '" ' . $commandLine;
         } else {
-            $commandLine = $gitCommandLine;
             array_unshift($commandLine, $gitBinary);
         }
 
         // Resolve the working directory of the Git process. Use the directory
         // in the command object if it exists.
-        if ($cwd === null) {
-            $directory = $gitCommand->getDirectory();
-            if ($directory !== null) {
-                if (! $cwd = realpath($directory)) {
-                    throw new GitException('Path to working directory could not be resolved: ' . $directory);
-                }
-            }
+        if ($cwd === null && ($directory = $gitCommand->getDirectory()) !== null &&
+            ! $cwd = realpath($directory)) {
+            throw new GitException('Path to working directory could not be resolved: ' . $directory);
         }
 
         // Finalize the environment variables, an empty array is converted
