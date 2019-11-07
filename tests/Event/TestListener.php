75c60e35cef1c479e6b3ef5afd02409a502ec21a
TomasVotruba <tomas.vot@gmail.com>
2019-11-07 18:22:01 +0100

    cleanup

--- tests/Event/TestListener.php
+++ tests/Event/TestListener.php
@@ -9,33 +9,18 @@ use GitWrapper\Event\AbstractGitEvent;
 final class TestListener
 {
     /**
-     * The methods that were called.
-     *
      * @var string[]
      */
     private $methods = [];
-
-    /**
-     * The event object passed to the onPrepare method.
-     *
-     * @var AbstractGitEvent
-     */
-    private $gitEvent;
 
     public function methodCalled(string $method): bool
     {
         return in_array($method, $this->methods, true);
     }
 
-    public function getEvent(): AbstractGitEvent
-    {
-        return $this->gitEvent;
-    }
-
     public function onPrepare(AbstractGitEvent $gitEvent): void
     {
         $this->methods[] = 'onPrepare';
-        $this->gitEvent = $gitEvent;
     }
 
     public function onSuccess(): void
