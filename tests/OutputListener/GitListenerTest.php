912a6639fb1d7b2ce38ba4530e4a1480bf461e10
TomasVotruba <tomas.vot@gmail.com>
2019-11-07 22:04:56 +0100

    change TestListener to more clear TestEventSubscriber

--- tests/OutputListener/GitListenerTest.php
+++ tests/OutputListener/GitListenerTest.php
@@ -4,59 +4,44 @@ declare(strict_types=1);
 
 namespace GitWrapper\Tests\OutputListener;
 
-use GitWrapper\Event\GitSuccessEvent;
-use GitWrapper\GitCommand;
 use GitWrapper\Tests\AbstractGitWrapperTestCase;
-use Symfony\Component\Process\Process;
 
 final class GitListenerTest extends AbstractGitWrapperTestCase
 {
-    public function testListener(): void
+    public function test(): void
     {
-        $listener = $this->addListener();
+        $eventSubscriber = $this->registerAndReturnEventSubscriber();
         $this->gitWrapper->version();
 
-        $this->assertTrue($listener->methodCalled('onPrepare'));
-        $this->assertTrue($listener->methodCalled('onSuccess'));
-        $this->assertFalse($listener->methodCalled('onError'));
-        $this->assertFalse($listener->methodCalled('onBypass'));
+        $this->assertTrue($eventSubscriber->wasMethodCalled('onPrepare'));
+        $this->assertTrue($eventSubscriber->wasMethodCalled('onSuccess'));
+        $this->assertFalse($eventSubscriber->wasMethodCalled('onError'));
+        $this->assertFalse($eventSubscriber->wasMethodCalled('onBypass'));
     }
 
-    public function testListenerError(): void
+    public function testError(): void
     {
-        $listener = $this->addListener();
+        $eventSubscriber = $this->registerAndReturnEventSubscriber();
         $this->runBadCommand(true);
 
-        $this->assertTrue($listener->methodCalled('onPrepare'));
-        $this->assertFalse($listener->methodCalled('onSuccess'));
-        $this->assertTrue($listener->methodCalled('onError'));
-        $this->assertFalse($listener->methodCalled('onBypass'));
+        $this->assertTrue($eventSubscriber->wasMethodCalled('onPrepare'));
+        $this->assertFalse($eventSubscriber->wasMethodCalled('onSuccess'));
+        $this->assertTrue($eventSubscriber->wasMethodCalled('onError'));
+        $this->assertFalse($eventSubscriber->wasMethodCalled('onBypass'));
     }
 
     public function testGitBypass(): void
     {
         $this->addBypassListener();
-        $listener = $this->addListener();
+        $eventSubscriber = $this->registerAndReturnEventSubscriber();
 
         $output = $this->gitWrapper->version();
 
-        $this->assertTrue($listener->methodCalled('onPrepare'));
-        $this->assertFalse($listener->methodCalled('onSuccess'));
-        $this->assertFalse($listener->methodCalled('onError'));
-        $this->assertTrue($listener->methodCalled('onBypass'));
+        $this->assertTrue($eventSubscriber->wasMethodCalled('onPrepare'));
+        $this->assertFalse($eventSubscriber->wasMethodCalled('onSuccess'));
+        $this->assertFalse($eventSubscriber->wasMethodCalled('onError'));
+        $this->assertTrue($eventSubscriber->wasMethodCalled('onBypass'));
 
         $this->assertEmpty($output);
-    }
-
-    public function testEvent(): void
-    {
-        $process = new Process([]);
-        $command = new GitCommand();
-
-        $event = new GitSuccessEvent($this->gitWrapper, $process, $command);
-
-        $this->assertSame($this->gitWrapper, $event->getWrapper());
-        $this->assertSame($process, $event->getProcess());
-        $this->assertSame($command, $event->getCommand());
     }
 }
